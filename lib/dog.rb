d1fd54a70c5d513e0468009e7cefbd054e85ee95
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-03 22:05:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
 # Your code goes here!
 class Dog
-  def name(name)
+  
+  def name=(name)
     @name=name
- end
-  def name
-    @name
- end
+  end
+ 
+   def name
+     @name
+   end
  
   def bark
     puts "woof!"
- end
+  end
  
  
 end
